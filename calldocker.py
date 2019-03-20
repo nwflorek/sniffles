598d6fac9ef21f2bcee332292853edfe78cc8f78
Nick Florek <nickflorek@gmail.com>
2019-03-20 16:05:32 -0500

    added env variables

--- calldocker.py
+++ calldocker.py
@@ -28,7 +28,7 @@ def call(command,cwd='',paths={},container='nwflorek/sniffles_tools',remove=True
     output = b''
     #try block to run the container
     try:
-        container_obj = client.containers.run(container,command,user=user,volumes=volumes,working_dir=cwd,remove=remove,detach=True)
+        container_obj = client.containers.run(container,command,user=user,volumes=volumes,working_dir=cwd,remove=remove,detach=True,environment=["BOWTIE2_INDEXES=/reference"])
     except:
         #loop through output as it is streamed
         for line in container_obj.logs(stream=True):
