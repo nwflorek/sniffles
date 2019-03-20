3902c2b7535a0f4ab0e0b7d790714449e0d98de4
Nick Florek <nickflorek@gmail.com>
2019-03-20 16:06:12 -0500

    added method to determine jobs and threads per job

--- sc.py
+++ sc.py
@@ -8,6 +8,22 @@ def checkexists(path):
     if not os.path.isdir(path):
         os.mkdir(path)
 
+def cpu_count(num):
+    if num <=1:
+        return 1,1
+    elif num <= 5:
+        return 1,num
+    else:
+        results = []
+        for n in range(2,num):
+            if num % n == 0:
+                results.append(n)
+        if len(results) < 2:
+            return cpu_count(num-1)
+        index = int(len(results)/2)
+        return results[index],int(num/results[index])
+
+
 def procTitle(title):
     title = ' '+title+' '
     rows,cols = os.popen('stty size','r').read().split()
