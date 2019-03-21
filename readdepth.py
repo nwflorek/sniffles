6e5e1baa8c3d0e36ec0c2bebfdbe89e0a74ee4c5
Nick Florek <nickflorek@gmail.com>
2019-03-21 10:21:25 -0500

    functional with docker

--- readdepth.py
+++ readdepth.py
@@ -2,18 +2,16 @@ import subprocess as sub
 import sys
 import os
 import shlex
+import calldocker as cd
 
-def average_depth(path):
-    if '.bam' in path:
-        cmd = f'samtools depth -a {path} | awk \'{{sum+=$3}} END {{ print "Average:",sum/NR}}\''
+def average_depth(file):
+    abs_path = os.path.abspath(file)
+    path = os.path.dirname(abs_path)
+    bam = os.path.basename(abs_path)
+
+    if '.bam' in bam:
+        cmd = f'bash -c "samtools depth -a {bam} | awk \'{{sum+=$3}} END {{ print sum/NR}}\'"'
     else:
-        return 'error: not a bam file'
-
-    p = sub.Popen(cmd,shell=True,stdout=sub.PIPE,stderr=sub.PIPE)
-    out,err = p.communicate()
-    try:
-        depth = out.decode('utf-8').strip().split(':')[1][1:]
-    except IndexError:
-        o = out.decode('utf-8')
-        return f'error: {o}'
-    return depth
+        return f'error: {bam} is not a bam file'
+    out = cd.call(cmd,'/data',{path:"/data"})
+    return out
