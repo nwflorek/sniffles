1ec23829dcabce17749710c7526c272970cc9584
Nick Florek <nickflorek@gmail.com>
2019-04-04 16:32:15 -0500

    removed varscan from snpcaller

--- snpcaller.py
+++ snpcaller.py
@@ -27,67 +27,45 @@ def snpcaller(runCFG,bam_files,threads='1'):
         caller = 'VarScan'
     print(f'\nSniffles: Started calling SNPs using {caller}')
 
-    cmds = []
+    bams = []
+    sample_list = []
     for path in bam_files:
         full_path = os.path.abspath(path)
         file_name = os.path.basename(full_path)
         path = os.path.dirname(full_path)
         id = file_name.split(".")[0]
-        #using lofreq
-        if caller == 'LoFreq':
-            #call snps
-            cmd1 = f'bash -c \'lofreq call -f /ref/{reference_sequence_name} -o {id}_lofreq.vcf /infile/{id}.bam && '
+        sample_list.append(id)
+        bams.append('/infile/'+file_name)
 
-            #filter snps
-            min_cov = runCFG['snpcalling']['minCoverage']
-            snp_qual_threshold = runCFG['snpcalling']['snpQualityThreshold']
-            snp_frequency = runCFG['snpcalling']['snpFrequency']
-            cmd2 = f'lofreq filter --cov-min {min_cov} --snvqual-thresh {snp_qual_threshold} --af-min {snp_frequency} -i {id}_lofreq.vcf -o {id}_snps.vcf \''
+    #generate mpileup
+    cmd1 = 'bash -c \'samtools mpileup -R -d 1000000 {bams} -f /ref/{reference_sequence_name} > all.mpileup &&'.format(bams=' '.join(bams),reference_sequence_name=reference_sequence_name)
 
-            #add commands to list for multiprocessing
-            cmds.append(cmd1+cmd2)
+    #call snps
+    snp_frequency=runCFG['snpcalling']['snpFrequency']
+    min_cov=runCFG['snpcalling']['minCoverage']
+    snp_qual_threshold=runCFG['snpcalling']['snpQualityThreshold']
 
-        #using varscan
-        if caller == 'VarScan':
-            #generate mpileup
-            cmd1 = f'bash -c \'samtools mpileup -d 1000000 /infile/{id}.bam -f /ref/{reference_sequence_name} > {id}_snps.pileup &&'
+    cmd2 = 'java -jar /tools/varscan.jar mpileup2cns all.mpileup --min-coverage {min_cov} --min-avg-qual {snp_qual_threshold} --min-var-freq {snp_frequency} --strand-filter 1 --output-vcf 1 --variants --vcf-sample-list <(echo -e "{samples}") > all_snps.vcf\''.format(min_cov=min_cov,snp_qual_threshold=snp_qual_threshold,snp_frequency=snp_frequency,samples='\n'.join(sample_list))
 
-            #call snps
-            snp_frequency=runCFG['snpcalling']['snpFrequency']
-            min_cov=runCFG['snpcalling']['minCoverage']
-            snp_qual_threshold=runCFG['snpcalling']['snpQualityThreshold']
+    #add commands to list for multiprocessing
+    cmd =cmd1+cmd2)
 
-            cmd2 = f'java -jar /tools/varscan.jar mpileup2cns {id}_snps.pileup --min-coverage {min_cov} --min-avg-qual {snp_qual_threshold} --min-var-freq {snp_frequency} --strand-filter 1 --output-vcf 1 > {id}_snps.vcf\''
-
-            #add commands to list for multiprocessing
-            cmds.append(cmd1+cmd2)
-
-        #command list for annotating aa changes
-        if runCFG['exec']['annotateAAChanges']:
-            pass
-            #TODO add annotater for annotating aa changes
-            #reference = reference.split('.')[0]
-            #cmd = f'java -jar {libPath}/snpeff/snpEff.jar {reference}.fasta {id}_snps.vcf'
-            #outfile = f'{id}_snps_annotated.vcf'
-            #annotate_cmds.append([cmd,outfile])
-
-    #execute multiprocessing commands
-    #start multiprocessing
-    pool = mp.Pool(processes=1)
+    #future code block for annotating aa changes
+    if runCFG['exec']['annotateAAChanges']:
+        pass
+        #TODO add annotater for annotating aa changes
 
     with open(logfile,'a') as outlog:
         outlog.write('***********\n')
         outlog.write('Calling SNPs\n')
-        results = pool.starmap_async(cd.call,[[cmd,'/outfile',{reference_sequence_dir:"/ref",path:"/infile",outDir:"/outfile"}] for cmd in cmds])
-        stdouts = results.get()
-        for stdout in stdouts:
-            outlog.write('-----------\n')
-            outlog.write(stdout)
+        results = cd.call(cmd,'/outfile',{reference_sequence_dir:"/ref",path:"/infile",outDir:"/outfile"})
+        outlog.write('-----------\n')
+        outlog.write(results)
         #denote end of logs
         outlog.write('***********\n')
 
     #get end time
     end = time.time()
     #get total runtime
-    runtime = end - start
+    runtime = round(end - start,2)
     print(f'\nSniffles: Finished calling snps in {runtime} seconds')
